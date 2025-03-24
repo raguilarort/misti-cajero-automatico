6c275157b3f18d191756c8c7fe44cc7ee294f6c4
RICARDO AGUILAR ORTEGA <ricardo.aguilar@senado.gob.mx>
2025-03-23 18:40:52 -0600

    first commit

---
+++ model/movimiento_bancario.py
@@ -0,0 +1,5 @@
+class MovimientoBancario:
+    def __init__(self, banco, ubicacion, total_efectivo):
+        self.banco = banco
+        self.ubicacion = ubicacion
+        self.total_efectivo = total_efectivo
